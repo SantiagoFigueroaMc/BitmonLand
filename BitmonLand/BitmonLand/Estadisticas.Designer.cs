bcda948bc6ea12008fb4821730d248a2c12448d8
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2019-05-30 12:12:22 -0400

    Estadisticas, falta aun

--- BitmonLand/BitmonLand/Estadisticas.Designer.cs
+++ BitmonLand/BitmonLand/Estadisticas.Designer.cs
@@ -28,12 +28,249 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox_bithala_especies = new System.Windows.Forms.ListBox();
+            this.label_tiempo_vida_promedio = new System.Windows.Forms.Label();
+            this.groupBox_tiempo_vida_promedio = new System.Windows.Forms.GroupBox();
+            this.groupBox_tiempo_vida_promedio_especie = new System.Windows.Forms.GroupBox();
+            this.listBox_tiempo_vida_promedio_especies = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox_tasa_bruta_natalidad_especie = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label_tasa_bruta_mortalidad = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox_cantidad_hijos_promedio_especie = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.listBox_especies_extintas = new System.Windows.Forms.ListBox();
+            this.groupBox_tiempo_vida_promedio.SuspendLayout();
+            this.groupBox_tiempo_vida_promedio_especie.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox_bithala_especies
+            // 
+            this.listBox_bithala_especies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_bithala_especies.FormattingEnabled = true;
+            this.listBox_bithala_especies.Location = new System.Drawing.Point(3, 16);
+            this.listBox_bithala_especies.Name = "listBox_bithala_especies";
+            this.listBox_bithala_especies.Size = new System.Drawing.Size(274, 180);
+            this.listBox_bithala_especies.TabIndex = 1;
+            // 
+            // label_tiempo_vida_promedio
+            // 
+            this.label_tiempo_vida_promedio.AutoSize = true;
+            this.label_tiempo_vida_promedio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_tiempo_vida_promedio.Location = new System.Drawing.Point(3, 16);
+            this.label_tiempo_vida_promedio.Name = "label_tiempo_vida_promedio";
+            this.label_tiempo_vida_promedio.Size = new System.Drawing.Size(13, 13);
+            this.label_tiempo_vida_promedio.TabIndex = 3;
+            this.label_tiempo_vida_promedio.Text = "0";
+            this.label_tiempo_vida_promedio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox_tiempo_vida_promedio
+            // 
+            this.groupBox_tiempo_vida_promedio.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox_tiempo_vida_promedio.Controls.Add(this.label_tiempo_vida_promedio);
+            this.groupBox_tiempo_vida_promedio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox_tiempo_vida_promedio.Location = new System.Drawing.Point(3, 3);
+            this.groupBox_tiempo_vida_promedio.MinimumSize = new System.Drawing.Size(170, 40);
+            this.groupBox_tiempo_vida_promedio.Name = "groupBox_tiempo_vida_promedio";
+            this.groupBox_tiempo_vida_promedio.Size = new System.Drawing.Size(278, 44);
+            this.groupBox_tiempo_vida_promedio.TabIndex = 4;
+            this.groupBox_tiempo_vida_promedio.TabStop = false;
+            this.groupBox_tiempo_vida_promedio.Text = "1 Tiempo promedio de vida";
+            // 
+            // groupBox_tiempo_vida_promedio_especie
+            // 
+            this.groupBox_tiempo_vida_promedio_especie.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox_tiempo_vida_promedio_especie.Controls.Add(this.listBox_tiempo_vida_promedio_especies);
+            this.groupBox_tiempo_vida_promedio_especie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox_tiempo_vida_promedio_especie.Location = new System.Drawing.Point(3, 53);
+            this.groupBox_tiempo_vida_promedio_especie.MinimumSize = new System.Drawing.Size(0, 40);
+            this.groupBox_tiempo_vida_promedio_especie.Name = "groupBox_tiempo_vida_promedio_especie";
+            this.groupBox_tiempo_vida_promedio_especie.Size = new System.Drawing.Size(278, 199);
+            this.groupBox_tiempo_vida_promedio_especie.TabIndex = 5;
+            this.groupBox_tiempo_vida_promedio_especie.TabStop = false;
+            this.groupBox_tiempo_vida_promedio_especie.Text = "2 Tiempo vida promedio por especies";
+            // 
+            // listBox_tiempo_vida_promedio_especies
+            // 
+            this.listBox_tiempo_vida_promedio_especies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_tiempo_vida_promedio_especies.FormattingEnabled = true;
+            this.listBox_tiempo_vida_promedio_especies.Location = new System.Drawing.Point(3, 16);
+            this.listBox_tiempo_vida_promedio_especies.Name = "listBox_tiempo_vida_promedio_especies";
+            this.listBox_tiempo_vida_promedio_especies.Size = new System.Drawing.Size(272, 180);
+            this.listBox_tiempo_vida_promedio_especies.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.listBox_tasa_bruta_natalidad_especie);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 258);
+            this.groupBox1.MinimumSize = new System.Drawing.Size(0, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 200);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "3 Tasa bruta natalidad por especie";
+            // 
+            // listBox_tasa_bruta_natalidad_especie
+            // 
+            this.listBox_tasa_bruta_natalidad_especie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_tasa_bruta_natalidad_especie.FormattingEnabled = true;
+            this.listBox_tasa_bruta_natalidad_especie.Location = new System.Drawing.Point(3, 16);
+            this.listBox_tasa_bruta_natalidad_especie.Name = "listBox_tasa_bruta_natalidad_especie";
+            this.listBox_tasa_bruta_natalidad_especie.Size = new System.Drawing.Size(272, 181);
+            this.listBox_tasa_bruta_natalidad_especie.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.listBox_bithala_especies);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(571, 53);
+            this.groupBox2.MinimumSize = new System.Drawing.Size(0, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(280, 199);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "7 Cantidad de bitmons por especie en Bithalla";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox3.Controls.Add(this.label_tasa_bruta_mortalidad);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(287, 3);
+            this.groupBox3.MinimumSize = new System.Drawing.Size(0, 40);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(278, 44);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "4 Tasa bruta de mortalidad";
+            // 
+            // label_tasa_bruta_mortalidad
+            // 
+            this.label_tasa_bruta_mortalidad.AutoSize = true;
+            this.label_tasa_bruta_mortalidad.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label_tasa_bruta_mortalidad.Location = new System.Drawing.Point(3, 16);
+            this.label_tasa_bruta_mortalidad.Name = "label_tasa_bruta_mortalidad";
+            this.label_tasa_bruta_mortalidad.Size = new System.Drawing.Size(13, 13);
+            this.label_tasa_bruta_mortalidad.TabIndex = 3;
+            this.label_tasa_bruta_mortalidad.Text = "0";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox4.Controls.Add(this.listBox_cantidad_hijos_promedio_especie);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(287, 53);
+            this.groupBox4.MinimumSize = new System.Drawing.Size(0, 40);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(278, 199);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "5 Cantidad de hijos en promedio por especie";
+            // 
+            // listBox_cantidad_hijos_promedio_especie
+            // 
+            this.listBox_cantidad_hijos_promedio_especie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_cantidad_hijos_promedio_especie.FormattingEnabled = true;
+            this.listBox_cantidad_hijos_promedio_especie.Location = new System.Drawing.Point(3, 16);
+            this.listBox_cantidad_hijos_promedio_especie.Name = "listBox_cantidad_hijos_promedio_especie";
+            this.listBox_cantidad_hijos_promedio_especie.Size = new System.Drawing.Size(272, 180);
+            this.listBox_cantidad_hijos_promedio_especie.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.groupBox5, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox2, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox_tiempo_vida_promedio, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox4, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox_tiempo_vida_promedio_especie, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox1, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox3, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(854, 461);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox5.Controls.Add(this.listBox_especies_extintas);
+            this.groupBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox5.Location = new System.Drawing.Point(287, 258);
+            this.groupBox5.MinimumSize = new System.Drawing.Size(0, 40);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(278, 200);
+            this.groupBox5.TabIndex = 8;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "6 Especies extintas";
+            // 
+            // listBox_especies_extintas
+            // 
+            this.listBox_especies_extintas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_especies_extintas.FormattingEnabled = true;
+            this.listBox_especies_extintas.Location = new System.Drawing.Point(3, 16);
+            this.listBox_especies_extintas.Name = "listBox_especies_extintas";
+            this.listBox_especies_extintas.Size = new System.Drawing.Size(272, 181);
+            this.listBox_especies_extintas.TabIndex = 1;
+            // 
+            // Estadisticas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(854, 461);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Estadisticas";
             this.Text = "Estadisticas";
+            this.groupBox_tiempo_vida_promedio.ResumeLayout(false);
+            this.groupBox_tiempo_vida_promedio.PerformLayout();
+            this.groupBox_tiempo_vida_promedio_especie.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.ListBox listBox_bithala_especies;
+        private System.Windows.Forms.Label label_tiempo_vida_promedio;
+        private System.Windows.Forms.GroupBox groupBox_tiempo_vida_promedio;
+        private System.Windows.Forms.GroupBox groupBox_tiempo_vida_promedio_especie;
+        private System.Windows.Forms.ListBox listBox_tiempo_vida_promedio_especies;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox_tasa_bruta_natalidad_especie;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label_tasa_bruta_mortalidad;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox_cantidad_hijos_promedio_especie;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ListBox listBox_especies_extintas;
     }
 }
--- BitmonLand/BitmonLand/Estadisticas.cs
+++ BitmonLand/BitmonLand/Estadisticas.cs
@@ -13,13 +13,67 @@ namespace BitmonLand
     public partial class Estadisticas : Form
     {
         // Los atributos de esta clase son las estadisticas que se van a guardar en el programa principal
-        
+        // Tiempo de vida promedio de los Bitmons.
+        int tiempo_vida_promedio;
+
+        // Tiempo de vida promedio de cada especie de Bitmon.
+        Dictionary<string, int> tiempo_vida_promedio_especie = new Dictionary<string, int>()
+        {
+            { "Gofue", 0 },
+            { "Taplan", 0 },
+            { "Doti", 0 },
+            { "Ent", 0 },
+            { "Dorvalo", 0 },
+            { "Wetar", 0 }
+        };
+
+        // Tasa bruta de natalidad de cada especie.
+        Dictionary<string, int> tasa_bruta_natalidad_especie = new Dictionary<string, int>()
+        {
+            { "Gofue", 0 },
+            { "Taplan", 0 },
+            { "Doti", 0 },
+            { "Ent", 0 },
+            { "Dorvalo", 0 },
+            { "Wetar", 0 }
+        };
+
+        // Tasa bruta de mortalidad.
+        int tasa_bruta_mortalidad_especie;
+
+        // Cantidad de hijos promedio por especie.
+        Dictionary<string, int> cantidad_hijos_promedio_especie = new Dictionary<string, int>()
+        {
+            { "Gofue", 0 },
+            { "Taplan", 0 },
+            { "Doti", 0 },
+            { "Ent", 0 },
+            { "Dorvalo", 0 },
+            { "Wetar", 0 }
+        };
+
+        // Lista de especies extintas, si las hay.
+        List<string> especies_extintas = new List<string>() { "No hay especies extintas" };
+
+        // Listado de bitmons en el bithala, cantidad por especie, mostrar en porcentaje.
+        Dictionary<string, int> bithala_cantidad_bitmons_especie = new Dictionary<string, int>()
+        {
+            { "Gofue", 0 },
+            { "Taplan", 0 },
+            { "Doti", 0 },
+            { "Ent", 0 },
+            { "Dorvalo", 0 },
+            { "Wetar", 0 }
+        };
+
+
+
+
 
         public Estadisticas()
         {
             InitializeComponent();
             // Aquí hay que settear las variables de arriba a los controles del form
         }
-        
     }
 }
