d45bc2d2c9a7502c7c85badb468d263de5bcf751
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2019-06-08 17:24:32 -0400

    Controlador y modelo

---
+++ BitmonLand/BitmonLand/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmonLand
+{
+    class Model
+    {
+        // llamar desde aquí a las funciones como moverse, atacar y reproducirse, ir al bithala, etc
+        // o algo similar
+    }
+}
